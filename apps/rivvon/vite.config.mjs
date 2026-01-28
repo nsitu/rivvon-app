44b39e41c21f2ca08e0dd39a577f4bd729de196e
Harold Sikkema <harold@nsitu.ca>
2026-01-28 13:30:47 -0500

    fix: change Vue build output to dist for deployment

--- apps/rivvon/vite.config.mjs
+++ apps/rivvon/vite.config.mjs
@@ -10,7 +10,7 @@ export default defineConfig({
     base: '/',
     build: {
         target: 'esnext',
-        outDir: 'dist-vue',
+        outDir: 'dist',
         rollupOptions: {
             input: {
                 main: resolve(__dirname, 'index.html'),
